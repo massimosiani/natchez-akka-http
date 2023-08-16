f834a10161ca66b9d86283077f038fffabe0b7ac
Massimo Siani <massimosiani@users.noreply.github.com>
2023-08-16 09:54:39 +0200

    bump dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,14 +1,14 @@
 object Dependencies {
 
   object versions {
-    val akka       = "2.6.20"
+    val akka       = "2.6.21"
     val akkaHttp   = "10.2.10"
-    val catsEffect = "3.5.0"
+    val catsEffect = "3.5.1"
     val log4cats   = "2.6.0"
     val log4j      = "2.20.0"
     val munit      = "0.7.29"
-    val natchez    = "0.3.2"
+    val natchez    = "0.3.3"
     val scalacheck = "1.17.0"
-    val tapir      = "1.5.1"
+    val tapir      = "1.7.0"
   }
 }
